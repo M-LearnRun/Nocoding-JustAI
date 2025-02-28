9d10a98b4c8cac7f96d756383d7d89d4d118caca
머신러너 <97908307+M-LearnRun@users.noreply.github.com>
2025-02-28 18:26:45 +0900

    Create #3-2-21.py
    CNN 딥러닝 이미지 분류 분석1 - 필요한 모듈 축약 불러오기 코드

---
+++ Chapter3-이과 직장인/02_바이오 개발자/#3-2-21.py
@@ -0,0 +1,10 @@
+import os
+import zipfile
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from PIL import Image
+import matplotlib.pyplot as plt
