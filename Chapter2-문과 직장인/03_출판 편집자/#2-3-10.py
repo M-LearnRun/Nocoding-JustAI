d0350b1be4d8593c8dd4d560f1e61f2113face36
머신러너 <97908307+M-LearnRun@users.noreply.github.com>
2025-02-28 17:48:39 +0900

    Create #2-3-10.py
    조건에 맞는 책 필터링 코드

--- Chapter2-문과 직장인/03_출판 편집자/#2-3-10.py
+++ Chapter2-문과 직장인/03_출판 편집자/#2-3-10.py
@@ -1,6 +1,7 @@
-# 다변량 EDA: 전체 히트맵 시각화
-plt.figure(figsize=(15, 12))
-corr = books_data_new.corr()  # 수치형 열에 대한 상관관계 계산
-sns.heatmap(corr, annot=True, cmap='coolwarm', linewidths=0.5, linecolor='gray')
-plt.title('전체 히트맵 (상관관계)')
-plt.show()
+filtered_essay_books = books_data_new[
+(books_data_new['대분류'].str.contains('에세이', na=False) |
+books_data_new['중분류'].str.contains('에세이', na=False) |
+books_data_new['소분류'].str.contains('에세이', na=False)) &
+(books_data_new['번역서원제'].isnull()) & # 번역서가 아닌 책
+(books_data_new['총_리뷰_개수'] >= 10) # 100자평개수와 마이리뷰개수의 합이 10개 이상인 책
+]
