2a4167ca493bdd1be2685af94ee584f943075fdb
머신러너 <97908307+M-LearnRun@users.noreply.github.com>
2025-02-28 17:59:48 +0900

    Create #2-4-1.py
    파이썬 라이브러리 설치 코드

---
+++ Chapter2-문과 직장인/04_펀드 매니저/#2-4-1.py
@@ -0,0 +1,4 @@
+# 업로드된 wheel 파일 순차적으로 설치 코드
+!pip install /mnt/data/koreanize_matplotlib-0.1.1-py3-none-any.whl
+!pip install /mnt/data/finance_datareader-0.9.94-py3-none-any.whl
+!pip install /mnt/data/mplfinance-0.12.10b0-py3-none-any.whl
